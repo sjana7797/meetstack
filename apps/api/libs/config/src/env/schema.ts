f7a2b4f619eace5b6eedc0d2faffc8e044601187
sjana7797 <sjana7797@gmail.com>
2026-08-16 19:23:49 +0530

    fix(api): updating env schema

--- apps/api/libs/config/src/env/schema.ts
+++ apps/api/libs/config/src/env/schema.ts
@@ -7,15 +7,38 @@ const mediaServiceSchema = z.object({
   MEDIA_MIN_PORT: z.coerce.number().default(40000),
   MEDIA_MAX_PORT: z.coerce.number().default(40100),
 });
+const databaseSchema = z.object({
+  DATABASE_URL: z.url(),
+});
+const authSchema = z.object({
+  BETTER_AUTH_SECRET: z.string().min(32),
+  BETTER_AUTH_URL: z.url(),
+});
+const redisSchema = z.object({
+  REDIS_HOST: z.string().min(1).default("localhost"),
+  REDIS_PORT: z.coerce.number().default(6379),
+  REDIS_PASSWORD: z.string().optional(),
+  REDIS_DB: z.coerce.number().default(0),
+  REDIS_NAMESPACE: z.string().min(1).default("meetstack"),
+});
+const bullMqSchema = z.object({
+  QUEUE_CONCURRENCY: z.coerce.number().default(5),
+
+  // Bull board
+  BULL_BOARD_USER: z.string().min(1),
+  BULL_BOARD_PASSWORD: z.string().min(8),
+});
+const apiSchema = z.object({
+  PORT: z.coerce.number().int().positive().default(5000),
+  TRUSTED_PROXIES: z.string().default(""),
+  CORS_ORIGINS: z.string().default(""),
+});
 
 export const envSchema = z
   .object({
-    PORT: z.coerce.number().int().positive().default(5000),
     NODE_ENV: z
       .enum(["development", "production", "local"])
       .default("development"),
-    TRUSTED_PROXIES: z.string().default(""),
-    CORS_ORIGINS: z.string().default(""),
     PRETTY_LOGS: z
       .string()
       .default("false")
@@ -23,21 +46,12 @@ export const envSchema = z
     LOG_LEVEL: z
       .enum(["fatal", "error", "warn", "info", "debug", "trace"])
       .default("trace"),
-
-    // redis
-    REDIS_HOST: z.string().min(1).default("localhost"),
-    REDIS_PORT: z.coerce.number().default(6379),
-    REDIS_PASSWORD: z.string().optional(),
-    REDIS_DB: z.coerce.number().default(0),
-    REDIS_NAMESPACE: z.string().min(1).default("meetstack"),
-
-    /** Jobs processed concurrently per worker process. */
-    QUEUE_CONCURRENCY: z.coerce.number().default(5),
-
-    // Bull board
-    BULL_BOARD_USER: z.string().min(1),
-    BULL_BOARD_PASSWORD: z.string().min(8),
   })
-  .and(mediaServiceSchema);
+  .and(apiSchema)
+  .and(databaseSchema)
+  .and(redisSchema)
+  .and(bullMqSchema)
+  .and(mediaServiceSchema)
+  .and(authSchema);
 
 export type TEnv = z.infer<typeof envSchema>;
